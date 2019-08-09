60afc646e06f717a01fd01e4c3fea74a8c531477
chiveturkey <andersen.eric@gmail.com>
2019-08-09 16:07:56 -0500

    Add sleep to give time for image build to complete.

--- redis/build.sh
+++ redis/build.sh
@@ -8,6 +8,10 @@ done < ../etc/nextcloud.config
 docker build \
   --no-cache -t redis .
 
+# TODO: HACKTAG: There's some sort of race condition that causes this to fail if it executes too
+# soon.  It would be better to watch output from `docker image ls` or something.
+sleep 5
+
 docker run \
   -d \
   --name redis \
