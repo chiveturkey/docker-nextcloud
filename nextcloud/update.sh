961e2d76ec57731aa65131b8d196bcfadfad89c0
chiveturkey <andersen.eric@gmail.com>
2019-08-13 19:19:35 -0500

    Move php.ini tweaks out of shell scripts into Dockerfile.

--- nextcloud/update.sh
+++ nextcloud/update.sh
@@ -31,13 +31,6 @@ docker exec docker-nextcloud-nextcloud bash -c " \
   ln -s /nextcloud/config /var/www/html/nextcloud/config \
   && ln -s /nextcloud/data /var/www/html/nextcloud/data"
 
-# Tune PHP settings.
-docker exec docker-nextcloud-nextcloud bash -c " \
-  sed -i -e 's/memory_limit = 128M/memory_limit = 512M/'                                   /etc/opt/rh/rh-php72/php.ini \
-  && sed -i -e 's/opcache.max_accelerated_files=4000/opcache.max_accelerated_files=10000/' /etc/opt/rh/rh-php72/php.d/10-opcache.ini \
-  && sed -i -e 's/;opcache.save_comments=1/opcache.save_comments=1/'                       /etc/opt/rh/rh-php72/php.d/10-opcache.ini \
-  && sed -i -e 's/;opcache.revalidate_freq=2/opcache.revalidate_freq=1/'                   /etc/opt/rh/rh-php72/php.d/10-opcache.ini"
-
 # TODO: HACKTAG: There's some sort of race condition that causes this to fail if it executes too
 # soon.  It would be better to watch output from `docker ps` or something.
 sleep 5
